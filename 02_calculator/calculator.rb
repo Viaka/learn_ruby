5d903489bb1c2cb4032225040ee6e56125e735ad
Viaka <rharper9@gmail.com>
2017-11-01 02:44:56 +0000

    Exercises 1, 2, and most of 3

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,32 @@
 #write your code here
+def add(n1, n2)
+    n1 + n2
+end
+
+def subtract(n1, n2)
+    n1 - n2
+end
+
+def sum(numbers)
+    sum=0
+    numbers.each do |number|
+        sum+=number
+    end
+    return sum
+end
+
+def multiply(numbers)
+    sum=1
+    numbers.each do |number|
+        sum*=number
+    end
+    return sum
+end
+
+def power(n1, n2)
+    n1 ** n2
+end
+
+def factorial(num)
+    (1..num).reduce(:*) || 1
+end
--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,29 @@
 #write your code here
+def echo(phrase)
+    return phrase
+end
+
+def shout(phrase)
+    return phrase.upcase
+end
+
+def repeat(phrase, num=2)
+    sentence = ""
+    (0..num-1).each do |i|
+        sentence += phrase 
+        sentence += " "
+    end
+    return sentence.chop
+end
+
+def start_of_word(phrase, letters)
+    return phrase[0,letters]
+end
+
+def first_word(phrase)
+    return phrase[0,phrase.index(" ")]
+end
+
+def titleizer(phrase)
+    return phrase.titleize
+end
