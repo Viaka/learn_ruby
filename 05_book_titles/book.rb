61d4cbf5c0fda379196c14136523d44e69255aca
Viaka <rharper9@gmail.com>
2017-11-02 14:20:27 +0000

    Finished 3-6

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,14 @@
 class Book
-# write your code here
+  attr_reader :title
+
+  def title=(title)
+    @title = capitalizer(title)
+  end
+
+  private
+
+  def capitalizer(title)
+    ignore = %w[a an the and but or nor at by for from in into of off on onto out over up with to o']
+    title.capitalize.split.map { |w| ignore.include?(w) ? w : w.capitalize }.join(' ')
+  end
 end
